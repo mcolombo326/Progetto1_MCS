f3c32f91c8964492619cd89f37be245fc0f43bf0
mcolombo326 <102531894+mcolombo326@users.noreply.github.com>
2025-05-07 13:36:35 +0200

    jacobi finito in teoria

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import time
-from matrix_utils.config import MATRIX1_PATH, TOLERANCES
+
+from matrix_utils.config import MATRIX_PATH, TOLERANCES
 from matrix_utils.matrix_loader import prepare_system
 from solver.jacobi import jacobi
-import numpy as np
+
 
 def main():
-    # carica A e b dal file mtx
-    A, b = prepare_system(MATRIX1_PATH)
 
-    for tol in TOLERANCES:
-        print("Jacobi with tolerance:", tol)
-        start = time.time()
-        # chiama Jacobi
-        x_sol, err_rel, num_iter = jacobi(A, b, tol=tol)
-        end = time.time()
-        tempo = end - start
+    for path in MATRIX_PATH:
+        # carica A e b dal file mtx
+        A, b = prepare_system(path)
 
-        print("Errore Relativo:", err_rel)
-        print("Numero di iterazioni:", num_iter)
-        print("Tempo di calcolo (s):", tempo)
-        print("-" * 40)
+        print("\nMatrice:", path[4:])
+
+        for tol in TOLERANCES:
+            print("Jacobi with tolerance:", tol)
+            start = time.time()
+            # chiama Jacobi
+            x_sol, err_rel, num_iter = jacobi(A, b, tol=tol)
+            end = time.time()
+            tempo = end - start
+
+            print("Errore Relativo:", err_rel)
+            print("Numero di iterazioni:", num_iter)
+            print("Tempo di calcolo (s):", tempo)
+            print("-" * 40)
 
 
 if __name__ == "__main__":
